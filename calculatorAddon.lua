9d4ed0b5193c09c3cfd68b9d8b698e011256c2b5
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2024-12-29 02:31:52 -0600

    Create message

--- calculatorAddon.lua
+++ calculatorAddon.lua
@@ -1,1 +1,17 @@
-print("Nomo joto nomo culon")
+
+local function showMessage()
+    StaticPopupDialogs["JOTO_MESSAGE"] = {
+        text = "¡Nomo joto nomo culon!!!!",
+        button1 = "Acepto k soy joto",
+        OnAccept = function() end,
+        timeout = 0,
+        whileDead = true,
+        hideOnEscape = true
+    }
+
+    StaticPopup_Show("JOTO_MESSAGE")
+end
+
+-- Registrar el comando /joto
+SLASH_JOTO1 = "/joto"
+SlashCmdList["JOTO"] = mostrarMensaje
