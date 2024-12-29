5b916c530ee01fd2c0a689666783c07a66849a96
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2024-12-29 03:59:16 -0600

    Update and rename calculatorAddon.lua to calculator.lua

--- calculator.lua
+++ calculator.lua
@@ -17,19 +17,16 @@ SlashCmdList["NOMO"] = function()
 end
 
 local function onEvent(self, event, arg1)
-    -- Evento de subir de nivel
     if event == "PLAYER_LEVEL_UP" then
         showMessage("¡Felicidades por subir de nivel! Ahora eres un pinchi joto de nivel " .. arg1 .. "!")
     end
 
-    -- Evento de muerte del jugador (cambiamos a PLAYER_DEAD)
     if event == "PLAYER_DEAD" then
         showMessage("¡Te mataron! ¡Como eres de pendejo!")
     end
 end
 
--- Crear el frame y registrar eventos
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LEVEL_UP")  -- Registrar evento de subir de nivel
-frame:RegisterEvent("PLAYER_DEAD")      -- Registrar evento de muerte del jugador
-frame:SetScript("OnEvent", onEvent)     -- Asignar la función de manejo de eventos
+frame:RegisterEvent("PLAYER_LEVEL_UP") 
+frame:RegisterEvent("PLAYER_DEAD")   
+frame:SetScript("OnEvent", onEvent) 
