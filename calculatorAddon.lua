2f9e1c7fd62f389828c94ffd728a88c30cbaf1b1
Moi Baldenegro <124749789+MoiBaldenegro@users.noreply.github.com>
2024-12-29 03:15:11 -0600

    Update calculatorAddon.lua

--- calculatorAddon.lua
+++ calculatorAddon.lua
@@ -1,6 +1,6 @@
-local function showMessage()
+local function showMessage(text)
     StaticPopupDialogs["JOTO_MESSAGE"] = {
-        text = "¡Nomo joto nomo culon, subiste de nivel pinchi joto cabron!!!!",
+        text = text,
         button1 = "Acepto k me gusta la verga",
         OnAccept = function() end,
         timeout = 0,
@@ -11,13 +11,22 @@ local function showMessage()
     StaticPopup_Show("JOTO_MESSAGE")
 end
 
+SLASH_NOMO1 = "/nomo"
+SlashCmdList["NOMO"] = function()
+    showMessage("¡Nomo joto nomo culon!!!!")
+end
+
 local function onEvent(self, event, arg1)
     if event == "PLAYER_LEVEL_UP" then
-        showMessage()  
+        showMessage("¡Felicidades por subir de nivel! Ahora eres un pinchi joto de nivel " .. arg1 .. "!")
+    end
+
+    if event == "UNIT_DIED" and arg1 == "player" then
+        showMessage("¡Te mataron! ¡Como eres de pendejo!")
     end
 end
 
 local frame = CreateFrame("Frame")
-frame:RegisterEvent("PLAYER_LEVEL_UP") 
+frame:RegisterEvent("PLAYER_LEVEL_UP")  
+frame:RegisterEvent("UNIT_DIED") 
 frame:SetScript("OnEvent", onEvent)  
-
